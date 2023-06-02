ad04157f988b2843c207c14311a44246e2cf7faa
Xiaojing Deng <8redfishcreative@gmail.com>
2023-06-02 10:16:21 -0400

    create a form

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,16 +1,32 @@
 package com.ltp.gradesubmission;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
+    List<Grade> studentGrades = Arrays.asList(
+        new Grade("Harry", "Potions", "C-"),
+        new Grade("Hermione", "Arithamcy", "A+"),
+        new Grade("Neville", "Charms", "A-")
+    );
+
+    @GetMapping("/")
+    public String gradeForm(Model model){
+        model.addAttribute("grade", new Grade());
+        return "form";
+    }
+
     @GetMapping("/grades")
     public String getGrades(Model model){
-        Grade grade = new Grade("Harry", "Potions", "C-");
-        model.addAttribute("grade", grade);
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
     
+    
+
 }
