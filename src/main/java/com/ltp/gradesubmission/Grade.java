ec3c315f6b1906ef488f638936940e9b68ebbc89
Xiaojing Deng <8redfishcreative@gmail.com>
2023-06-02 10:47:20 -0400

    handle submit form

--- src/main/java/com/ltp/gradesubmission/Grade.java
+++ src/main/java/com/ltp/gradesubmission/Grade.java
@@ -38,4 +38,5 @@ public class Grade {
         this.score = score;
     }
 
+
 }
--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,19 +1,17 @@
 package com.ltp.gradesubmission;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GradeController {
-    List<Grade> studentGrades = Arrays.asList(
-        new Grade("Harry", "Potions", "C-"),
-        new Grade("Hermione", "Arithamcy", "A+"),
-        new Grade("Neville", "Charms", "A-")
-    );
+    List<Grade> studentGrades = new ArrayList<>();
 
     @GetMapping("/")
     public String gradeForm(Model model){
@@ -21,6 +19,12 @@ public class GradeController {
         return "form";
     }
 
+    @PostMapping("/handleSubmit")
+    public String submitGrade(Grade grade){
+      studentGrades.add(grade);
+      return "redirect:/grades";
+    }
+    
     @GetMapping("/grades")
     public String getGrades(Model model){
         model.addAttribute("grades", studentGrades);
